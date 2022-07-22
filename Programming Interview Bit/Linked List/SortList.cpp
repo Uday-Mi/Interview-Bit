fddac1165af9b38f87cb800d72df01cdb48946ee
Uday Mittal <91343270+Uday-Mi@users.noreply.github.com>
2022-07-22 13:53:15 +0530

    Update SortList.cpp

--- Programming Interview Bit/Linked List/SortList.cpp
+++ Programming Interview Bit/Linked List/SortList.cpp
@@ -53,3 +53,112 @@ ListNode* Solution::sortList(ListNode* A) {
 }
 
 // METHOD 2
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ListNode *merge (ListNode *a, ListNode *b);
+
+ListNode* Solution::sortList(ListNode* A) {
+    
+    ListNode *head = A;
+    ListNode *fast = head -> next;
+    ListNode *mid = head;
+    
+    if (A == NULL)
+    return A;
+    
+    if (A -> next == NULL)
+    return A;
+    
+    while (fast !=  NULL)
+    {        
+        fast = fast -> next;
+        if (fast != NULL)
+        {
+            mid = mid -> next;
+            fast = fast -> next;
+        }
+    }
+    
+    ListNode *a = head;
+    ListNode *b = mid -> next;
+    mid -> next = NULL;
+    
+    a = sortList(a);
+    b = sortList(b);
+    
+    if (a == NULL)
+    return b;
+    
+    if (b == NULL)
+    return a;
+    
+    return merge(a, b);
+    
+}
+
+ListNode *merge (ListNode *a, ListNode *b)
+{
+    if (a == NULL)
+    return b;
+
+    if (b == NULL)
+    return a;
+    
+    ListNode *list;
+    
+    if (a -> val < b -> val)
+    {
+        list = a;
+        a = a -> next;
+    }
+    
+    else
+    {
+        list = b;
+        b = b -> next;
+    }
+    
+    ListNode *ptr = list;
+    
+    while (a != NULL && b != NULL)
+    {
+        if (a -> val <= b -> val)
+        {
+            list -> next = a;
+            a = a-> next;
+            list = list -> next;
+        }
+        
+        else
+        {
+            list -> next = b;
+            b = b -> next;
+            list = list -> next;
+        }
+    }
+    
+    while (a != NULL)
+    {
+        list -> next = a;
+        a = a -> next;
+        list = list -> next;
+    }
+    
+    while (b != NULL)
+    {
+        list -> next = b;
+        b = b -> next;
+        list = list -> next;
+    }
+    
+    return ptr;
+}
