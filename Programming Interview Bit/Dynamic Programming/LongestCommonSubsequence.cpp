83f51413391fd20d3f99bc4acf24bb56da59ccb6
Uday Mittal <91343270+Uday-Mi@users.noreply.github.com>
2022-08-01 12:57:49 +0530

    Update LongestCommonSubsequence.cpp

--- Programming Interview Bit/Dynamic Programming/LongestCommonSubsequence.cpp
+++ Programming Interview Bit/Dynamic Programming/LongestCommonSubsequence.cpp
@@ -41,6 +41,8 @@ Explanation 1:
  The longest common subsequence is "bbcgf", which has a length of 5
  **/
 
+// METHOD 1 -- RECURSION WITH MEMOIZATION
+
 int t[1006][1006];
 
 int LCS (string a, string b, int n, int m)
@@ -66,5 +68,41 @@ int Solution::solve(string A, string B) {
     memset (t, -1, sizeof(t));
     
     return LCS(A, B, n, m);
+    
+}
+
+
+// METHOD 2 -- DYNAMIC PROGRAMMING
+
+int Solution::solve(string A, string B) {
+    
+    int n = A.size();
+    int m = B.size();
+    
+    int t[n + 1][m + 1];
+    
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+            t[i][j] = 0;
+        }
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (A[i - 1] == B[j - 1])
+            t[i][j] = t[i - 1][j - 1] + 1;
+            
+            else
+            {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        }
+    }
+    return t[n][m];
     
 }
